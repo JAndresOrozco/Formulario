20e054e227711404d8c668c375d9b2cad82525f4
Andres Orozco <madrid_koz@hotmail.com>
2019-07-02 20:16:09 -0500

    Formulario

---
+++ jq.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        var myArray = [ '#nombre', '#fem || #masc', '#edad', '#carrera', 'state' ];
+        $('#myarray').appendto('table');
+
+    })
+})
