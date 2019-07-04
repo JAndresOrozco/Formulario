9b915f703365fedee9054a3d7b1c299cf0d34e64
Andres Orozco <madrid_koz@hotmail.com>
2019-07-03 23:35:50 -0500

    Form

--- jq.js
+++ jq.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
-    $("button").click(function(){
-        var myArray = [ '#nombre', '#fem || #masc', '#edad', '#carrera', 'state' ];
-        $('#myarray').appendto('table');
+    $('#aceptar').click(function(){
+          var nombre = $('#nombre').val();
+          var sexo = $('input:radio[name=sexo]:checked').val();
+          var edad = $('#edad').val();
+          var carrera = $("#Carrera").val();
+          var estado = $("#state").val();
+      
+       var tbody = $('tbody');
+       var tr = $('<tr></tr>');
+       var form = [nombre, sexo, edad,carrera,estado];
+       var items = [];
 
-    })
-})
+       form.forEach(function(i,x){
+           var td = $('<td>' + form[x] + '</td>');
+           items.push(td); 
+       });
+       tr.append(items);
+       tbody.append(tr);
+    });
+});
